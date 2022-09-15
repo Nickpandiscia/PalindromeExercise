461dc569d8ecbfd16f8a02111384c1f302ad4943
Nickpandiscia <npandiscia1@gmail.com>
2022-09-15 14:36:29 -0400

    initial commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using PalindromeExercise;
+using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void MyWord(string word, bool expected)
         {
-
+            var theTest = new Palindrome();
+            var actual = theTest.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
